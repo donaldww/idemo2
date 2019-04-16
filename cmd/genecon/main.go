7e6784d6833418838cf82a4c0b0bd6e9d2eae80b
Donald Wilson <donald.ww@me.com>
2019-04-16 08:08:23 -0400

    Initial commit

---
+++ cmd/genecon/main.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+import "math/rand"
+import "time"
+
+const numberOfNodes = 13
+
+func main() {
+	var result = randomList(numberOfNodes, len(ids))
+	// fmt.Println(result)
+	printHeader()
+	printGroup(result, ids)
+	fmt.Println()
+
+	// printAll()
+}
+
+/*
+func printAll() {
+	for i := 0; i < 500; i++ {
+		fmt.Println(i+1, ids[i])
+	}
+}
+*/
+
+func printGroup(searchResult []int, names NodeID) {
+
+	leader := randomLeader()
+	for i, key := range searchResult {
+		if i == leader {
+			fmt.Print("    LEADER = ")
+		}
+		fmt.Println(names[key])
+	}
+}
+
+func printHeader() {
+	fmt.Println()
+	fmt.Println(`IG17 Consensus Group Randomizer
+-------------------------------
+
+- A new consensus group and leader is
+randomly generated for every group of
+transactions added to the IG17 blockchain.
+
+- The Randomizer is protected by the SGX
+trusted execution environment (TEE).
+
+- IG17 uses RAFT consensus algorithm because
+it supports fault-tolerance and high performance.`)
+	fmt.Println()
+}
+
+func randomLeader() int {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	return r1.Intn(numberOfNodes)
+}
+
+func randomList(n, max int) []int {
+	result := make([]int, n)
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	for i := 0; i < n; i++ {
+		result[i] = r1.Intn(max)
+	}
+	return result
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/donaldww/idemo
+
+go 1.12
