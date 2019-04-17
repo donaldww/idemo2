04a65226351ed4a5733627c222896d6ce1bb9405
Donald Wilson <donald.ww@me.com>
2019-04-17 08:13:33 -0400

    Send output to /dev/null

--- shutdown.sh
+++ shutdown.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pkill cockroach
+pkill cockroach > /dev/null
 echo "Shutting down idemo"
 sleep 1
 ps -axf | grep "[c]ockroach"
--- startup.sh
+++ startup.sh
@@ -18,16 +18,16 @@ mkdir -p ${IPATH}/tmp
 # Start cockroachdb
 cd ${IPATH}/db
 echo ; echo "Starting cockroach db"
-cockroach start --insecure --listen-addr=localhost 2>/dev/null &
+cockroach start --insecure --listen-addr=localhost 2>1 > /dev/null &
 cockroach start --insecure --store=node2 --listen-addr=localhost:26258 --http-addr=localhost:8081 \
---join=localhost:26257 2>/dev/null &
+--join=localhost:26257 2>1 > /dev/null &
 cockroach start --insecure --store=node3 --listen-addr=localhost:26259 --http-addr=localhost:8082 \
---join=localhost:26257 2>/dev/null &
+--join=localhost:26257 2>1 > /dev/null &
 
 # Show cockroach nodes
 ps -axf | grep "[c]ockroach"
 
 # TODO: setup bin directory
 
-cd ${IPATH}/tmp
+cd ${IPATH}/tmpcd
 echo ; echo "Ready to start idemo"
